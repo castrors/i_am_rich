51da767b9cde655fa0c821e71d2b77467a466129
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2019-05-24 13:49:20 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
